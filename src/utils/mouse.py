caee2f819bc383dc24a9e466f9a22b4f5eaaa205
Samoray.l337 <Samoray.l337@gmail.com>
2022-03-11 16:16:57 +0200

    add: TODO

--- src/utils/mouse.py
+++ src/utils/mouse.py
@@ -1,6 +1,7 @@
 import curses
 
 
+# TODO: this function will capture clicks on all the line, not specefic on the wanted element
 def get_pressed_mouse_element(stdscr: 'curses._CursesWindow'):
     _, _, my, _, _ = curses.getmouse()
     _, w = stdscr.getmaxyx()
